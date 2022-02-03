e63a80c49ebf0d0a3946782918233a0fa2cdbf85
summerrain <cohen.carryl@gmail.com>
2022-02-03 05:37:56 -0500

    .

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("orbital-ability-340123-aae473355202.json")
+  credentials = file("C:\Users\family\Downloads\orbital-ability-340123-aae473355202.json")
   project     = "orbital-ability-340123"
   region      = "us-east4-a"
 }
